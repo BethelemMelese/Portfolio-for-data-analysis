773411fe2e8aa93283f76c7c8d1c1a094922527c
Bethelem Melese <bethelem.m@ambftradingplc.com>
2024-09-02 22:53:03 +0300

    Rebuild the frontend to update the url

--- server-side/index.js
+++ server-side/index.js
@@ -31,7 +31,8 @@ app.use(
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization,Origin, X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization,Origin, X-Requested-With');
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   // res.header(
   //   "Access-Control-Allow-Headers",
   //   "Origin, X-Requested-With, Content-Type, Accept, Authorization, 'Content-Type' : 'multipart/form-data' ,* "
