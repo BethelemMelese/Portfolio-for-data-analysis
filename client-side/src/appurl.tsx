8f388d255771a0e6362e7b05b37b589c4e789094
Bethelem Melese <bethelem.m@ambftradingplc.com>
2024-09-02 16:21:47 +0300

    Update the File Upload funcation and Main Content

--- client-side/src/appurl.tsx
+++ client-side/src/appurl.tsx
@@ -1,4 +1,5 @@
 import React from "react";
-export const appUrl = "http://localhost:5000/api/";
+// export const appUrl = "http://localhost:5000/api/";
+export const appUrl="https://portfolio-for-data-analysis.vercel.app/";
 
 export const token = localStorage.getItem("token");
